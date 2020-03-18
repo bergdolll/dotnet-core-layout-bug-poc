86500ecee9ddbe6662082981aa8df182c4a94636
Lukas Bergdoll <lukas.bergdoll@mvtec.com>
2020-03-18 13:29:14 +0100

    Add POC

---
+++ Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace layout_bug_poc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Drawing.Size ExpectedSize = new System.Drawing.Size(538, 558);
+
+            this.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = ExpectedSize;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            System.Diagnostics.Debug.Assert(this.ClientSize == ExpectedSize);
+        }
+
+        #endregion
+    }
+}
